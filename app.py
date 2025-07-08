ef0008c76a0129b20ac5e56deb499371ea577a85
Prakhar Srivastava <prakharsrivastava019@gmail.com>
2025-07-08 11:32:37 +0530

    Directory Updated

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title("Simple Streamlit App")
-
-name = st.text_input("Enter your name")
-st.write(f"Hello, {name}!")
-
-x = st.slider("Select a number", 0, 100, 25)
-st.write(f"Square of the number is: {x * x}")
